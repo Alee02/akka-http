0b04e73d9484a58033733bb01e44bda516062ea3
Alee02 <a-shah@hotmail.co.uk>
2019-05-19 22:25:43 +0100

    Created a basic low level http server with akka

--- src/main/scala/com/dp/LowLevelApi.scala
+++ src/main/scala/com/dp/LowLevelApi.scala
@@ -1,5 +1,78 @@
 package com.dp
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.IncomingConnection
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Sink
+
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
 object LowLevelApi extends App {
 
+  implicit val system = ActorSystem("LowLevelServerAPI")
+  implicit val materializer = ActorMaterializer()
+  import system.dispatcher
+
+  val serverSource = Http().bind("localhost", 8003)
+  val connectionSink = Sink.foreach[IncomingConnection] { connection =>
+    println(s"Accepted incoming connection from: ${connection.remoteAddress}")
+  }
+
+  val serverBindingFuture = serverSource.to(connectionSink).run()
+
+  serverBindingFuture.onComplete{
+    case Success(binding) => {
+      println("Server binding succesful")
+      binding.terminate(2 seconds)
+    }
+    case Failure(ex) => println(s"Server binding failed: $ex")
+  }
+
+  /*
+  Method 1: Synchronously server HTTP responses
+   */
+
+  val requestHandler: HttpRequest => HttpResponse = {
+    case HttpRequest(HttpMethods.GET, _, _, _, _) =>
+      HttpResponse(
+        StatusCodes.OK, // 200
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            | <body>
+            | Hello from Akka HTTP!
+            | </body>
+            |</html>
+          """.stripMargin
+        )
+      )
+    case request: HttpRequest =>
+      request.discardEntityBytes()
+      HttpResponse(
+        StatusCodes.NotFound,
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            | <body>
+            | OOPS! The Resource can't be found
+            | </body>
+            |</html>
+          """.stripMargin
+        )
+      )
+  }
+
+  val httpSyncConnectionHandler = Sink.foreach[IncomingConnection] { connection =>
+      connection.handleWithSyncHandler(requestHandler)
+  }
+
+  Http().bind("localhost", 8000).runWith(httpSyncConnectionHandler)
+
+  // Shorthand same as above
+  Http().bindAndHandleSync(requestHandler, "localhost", 8001)
 }
