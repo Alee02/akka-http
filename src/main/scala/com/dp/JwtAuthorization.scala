ff82aee5a448e941be095e317bc724a1cda793e2
Alee02 <a-shah@hotmail.co.uk>
2019-06-09 12:33:14 +0100

    Created login and auth routes with potential itility methods for jwt token

--- src/main/scala/com/dp/JwtAuthorization.scala
+++ src/main/scala/com/dp/JwtAuthorization.scala
@@ -2,6 +2,8 @@ package com.dp
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.{HttpResponse, StatusCode, StatusCodes}
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
 import spray.json._
@@ -18,4 +20,41 @@ object JwtAuthorization extends App with SprayJsonSupport{
   import system.dispatcher
   import SecurityDomain._
 
+  val superSecretPasswordDb = Map(
+    "admin" -> "admin",
+    "ali" -> "scalaRocks1"
+  )
+
+  def checkPassword(username: String, password: String): Boolean = ???
+
+  def createToken(username: String, experiationPeriodInDays: Int): String  = ???
+
+  val loginRoute =
+    post {
+      entity(as[LoginRequest]) {
+        case LoginRequest(username, password) if checkPassword(username, password) =>
+          // only use token for 1 day
+          val token = createToken(username, 1)
+          // directive that allows http response to use http header
+          respondWithHeader(RawHeader("Access-Token", token)) {
+            complete(StatusCodes.OK)
+          }
+        case _ => complete(StatusCodes.Unauthorized)
+      }
+    }
+
+  def isTokenExpired(token: String): Boolean = ???
+
+  def isTokenValid(token: String): Boolean= ???
+
+  val authenticatedRoute =
+    (path("secureEndpoint") & get) {
+      optionalHeaderValueByName("Authorization") {
+        case Some(token) if isTokenExpired(token) =>
+          complete(HttpResponse(status = StatusCodes.Unauthorized, entity = "Token expired."))
+        case Some(token) if isTokenValid(token) =>
+          complete("User accessed authorized endpoint!")
+        case _ => complete(HttpResponse(status = StatusCodes.Unauthorized, entity = "Token is invalid, or has been tampered with"))
+      }
+    }
 }
